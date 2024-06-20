9a9e345e02068902b5909e632afb16961c268538
GaLaXyILY <sonnda321@gmail.com>
2024-06-21 02:08:50 +0700

    a

--- src/main/java/com/ticxo/modelengine/api/events/ModelDismountEvent.java
+++ src/main/java/com/ticxo/modelengine/api/events/ModelDismountEvent.java
@@ -1,5 +1,6 @@
 package com.ticxo.modelengine.api.events;
 
+import com.ticxo.modelengine.api.events.AbstractEvent;
 import com.ticxo.modelengine.api.model.ActiveModel;
 import com.ticxo.modelengine.api.model.bone.type.Mount;
 import org.bukkit.entity.Entity;
@@ -7,52 +8,54 @@ import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
-public class ModelDismountEvent extends AbstractEvent implements Cancellable {
-   private static final HandlerList handlers = new HandlerList();
-   private final ActiveModel vehicle;
-   private final Entity passenger;
-   private final boolean isDriver;
-   private final Mount seat;
-   private boolean cancelled;
+public class ModelDismountEvent
+extends AbstractEvent
+implements Cancellable {
+    private static final HandlerList handlers = new HandlerList();
+    private final ActiveModel vehicle;
+    private final Entity passenger;
+    private final boolean isDriver;
+    private final Mount seat;
+    private boolean cancelled;
 
-   @NotNull
-   public static HandlerList getHandlerList() {
-      return handlers;
-   }
+    @NotNull
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
 
-   @NotNull
-   public HandlerList getHandlers() {
-      return handlers;
-   }
+    @NotNull
+    public HandlerList getHandlers() {
+        return handlers;
+    }
 
-   public ActiveModel getVehicle() {
-      return this.vehicle;
-   }
+    public ActiveModel getVehicle() {
+        return this.vehicle;
+    }
 
-   public Entity getPassenger() {
-      return this.passenger;
-   }
+    public Entity getPassenger() {
+        return this.passenger;
+    }
 
-   public boolean isDriver() {
-      return this.isDriver;
-   }
+    public boolean isDriver() {
+        return this.isDriver;
+    }
 
-   public Mount getSeat() {
-      return this.seat;
-   }
+    public Mount getSeat() {
+        return this.seat;
+    }
 
-   public boolean isCancelled() {
-      return this.cancelled;
-   }
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
 
-   public void setCancelled(boolean cancelled) {
-      this.cancelled = cancelled;
-   }
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
 
-   public ModelDismountEvent(ActiveModel vehicle, Entity passenger, boolean isDriver, Mount seat) {
-      this.vehicle = vehicle;
-      this.passenger = passenger;
-      this.isDriver = isDriver;
-      this.seat = seat;
-   }
+    public ModelDismountEvent(ActiveModel vehicle, Entity passenger, boolean isDriver, Mount seat) {
+        this.vehicle = vehicle;
+        this.passenger = passenger;
+        this.isDriver = isDriver;
+        this.seat = seat;
+    }
 }
